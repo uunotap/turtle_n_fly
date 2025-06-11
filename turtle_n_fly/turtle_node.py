822bbb476e6d113b5deb4d26fcb3be34dec06d48
Bigmist44 <166009165+Bigmist44@users.noreply.github.com>
2025-06-12 01:02:49 +0300

    More turtlebot movement

--- turtle_n_fly/turtle_node.py
+++ turtle_n_fly/turtle_node.py
@@ -4,8 +4,10 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import LaserScan
 import sensor_msgs_py.point_cloud2 as pc2
 import math
+import numpy as np
 
 class TurtleNode(Node):
 	def __init__(self):
@@ -15,13 +17,56 @@ class TurtleNode(Node):
 		self.clock = Clock()
 		self.cmd_publisher = self.create_publisher(Twist, '/turtlebot3/cmd_vel', 10)
 		self.pc_subscription = self.create_subscription(PointCloud2, '/turtlebot3/lidar/point_cloud',self.pc_callback,10)
+		self.scan_subscription = self.create_subscription(LaserScan, '/scan',self.scan_callback,10)
 		
 		self.moving_duration = 30
 		self.moving = True
 		self.timer_period = 0.1
 		self.timer = self.create_timer(self.timer_period, self.timer_callback)
+
+		self.max_side = None
+		self.turning = False
+		self.front_clear_count = 0
 		
 		self.start_time = self.get_clock().now()
+
+	def scan_callback(self,msg):
+
+		if self.turning:
+			self.get_logger().info("WE here")
+			return
+		angle_inc = msg.angle_increment
+		angle_min = msg.angle_min
+		ranges = msg.ranges
+
+		left_index = int((1.5708-angle_min)/angle_inc)
+		right_index = int((-1.5708-angle_min)/angle_inc)
+
+		left_index = max (0,min(left_index,len(ranges)-1))
+		right_index =max(0,min(right_index,len(ranges)-1))
+
+		left_distance = ranges[left_index]
+		right_distance= ranges[right_index]
+
+		if not math.isinf(left_distance) and not math.isinf(right_distance):
+			if left_distance > right_distance:
+				self.max_side = "left"
+			else:
+				self.max_side ="right"
+		elif math.isinf(left_distance) and not math.isinf(right_distance):
+			self.max_side ="right"
+		elif math.isinf(right_distance) and not math.isinf(left_distance):
+			self.max_side="left"
+		else:
+			
+			self.get_logger().info("I CANT TURNNNN")
+			self.max_side = "left"
+
+			
+
+		self.get_logger().info(f'Left (90°): {left_distance:.2f} m, Right (-90°): {right_distance:.2f} m')
+		self.get_logger().info(f'The further side is {self.max_side}')
+
 
 	def clock_callback(self, msg):
 		self.clock = msg
@@ -46,6 +91,8 @@ class TurtleNode(Node):
 		distances= [(x**2 + y**2 +z**2)**0.5 for x, y, z, *_ in points]
 		
 		front_points = []
+		
+
 		for x,y,z in points:
 			distance = math.sqrt(x**2 + y**2)
 			angle = math.atan2(y,x)
@@ -54,17 +101,27 @@ class TurtleNode(Node):
 		if front_points:
 		
 			min_in_front = min(front_points)
-			self.get_logger().info(f"Min in front {min_in_front}")
-			if min_in_front > 0.3:
-					self.get_logger().info("Forward!")
-					twist2.angular.z = 0.0
-					twist2.linear.x = 0.2
-					self.cmd_publisher.publish(twist2)
+			self.get_logger().info(f"Min in front {min_in_front} ")
+			if min_in_front > 0.2:
+					self.front_clear_count += 1
+					print(f"Waiting {self.front_clear_count}")
+					if self.front_clear_count > 5:
+
+						self.turning = False
+						self.get_logger().info("Forward!")
+						twist2.angular.z = 0.0
+						twist2.linear.x = 0.2
+						self.cmd_publisher.publish(twist2)
+					else: 
+						self.get_logger().info("Waitign for clear front")
 			else:
-				self.get_logger().info("Turning!!")
+				self.front_clear_count = 0
+				self.turning = True
 				twist2.linear.x=0.0
-				twist2.angular.z = 1.0
+				twist2.angular.z=1.0 if self.max_side == "left" else -1.0
 				self.cmd_publisher.publish(twist2)
+
+
 		else:
 			self.get_logger().warn("Nothing visbile in the front")
 		
@@ -80,7 +137,7 @@ class TurtleNode(Node):
 		
 		self.get_logger().info(f"Point cloud with {len(points)} points. Minimum distance of: {min_dist:.2f}, Maximum distance of: {max_dist: .2f}") 
 		#self.get_logger().info(f"The front points: {front_points}")
-		if min_dist > 1.84:
+		if min_dist > 1.0:
 			self.get_logger().info("Found an opening!!!!!!!!!!!!!!!!!!!!!")
 			self.get_logger().info("Found an opening!!!!!!!!!!!!!!!!!!!!!")
 			self.get_logger().info("Found an opening!!!!!!!!!!!!!!!!!!!!!")
@@ -98,3 +155,4 @@ def main(args=None):
 	rclpy.spin(node)
 	node.destroy_node()
 	rclpy.shutdown()
+
